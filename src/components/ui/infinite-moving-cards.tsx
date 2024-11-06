dab5ca138c868aea30b39e23ffb3950fc5fda77c
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-11-06 19:11:37 +0000

    bg of moving cards

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -74,11 +74,7 @@ export const InfiniteMovingCards = ({
             >
                 {items.map((item) => (
                     <li
-                        className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
-                        style={{
-                            background:
-                                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
-                        }}
+                        className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px] bg-yellow-300"                       
                         key={item.name}
                     >
                         <blockquote>

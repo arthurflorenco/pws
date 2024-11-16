79714fac95ffc4d6e287050192e965e5e79cdb24
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-11-16 12:58:21 +0000

    "Updated NavMobile component to add 'bg-neutral-900' class when isOpen is true"

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import woman from '../assets/woman.jpeg'
 import Image from 'next/image'
 import Card from './ui/Card'
@@ -10,6 +10,8 @@ import '../styles/animations.css'
 
 
 export const About = () => {
+    const titleRef = useRef<HTMLParagraphElement>(null);
+    const textRef = useRef<HTMLParagraphElement>(null);
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
@@ -28,35 +30,49 @@ export const About = () => {
         gsap.fromTo('.cardTime', { opacity: 0 }, {
             opacity: 1,
             x: 0,
+            duration: 0.8,
             scrollTrigger: {
                 trigger: '.cardTime',
                 start: 'top 80%',
-                end: 'bottom 80%',
-                scrub: true,
             }
         })
 
         gsap.fromTo('.titleTime', { opacity: 0 }, {
             opacity: 1,
             x: 0,
+            duration: 0.5,
             scrollTrigger: {
                 trigger: '.titleTime',
                 start: 'top 80%',
-                end: 'bottom 70%',
-                scrub: true,
             }
         })
 
-        gsap.fromTo('.textTime', { opacity: 0 }, {
-            opacity: 1,
-            y: 0,
-            scrollTrigger: {
-                trigger: '.titleTime',
-                start: 'top 80%',
-                end: 'bottom 50%',
-                scrub: true,
-            }
-        })
+        // Animação do texto
+        const splitText = (element: HTMLParagraphElement) => {
+            const text = element.innerText.split('').map((char, index) => (
+                `<span class="char" key=${index}>${char}</span>`
+            )).join('');
+            element.innerHTML = text;
+        };
+
+        if (textRef.current) {
+            splitText(textRef.current);
+            gsap.fromTo(
+                textRef.current.children,
+                { opacity: 0, y: 50 },
+                {
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.01,
+                    duration: 1,
+                    scrollTrigger: {
+                        trigger: '.titleTime',
+                        start: 'top 80%',
+                        toggleActions: "play none none reverse",
+                    },
+                }
+            );
+        }
 
     }, [])
 
@@ -65,7 +81,7 @@ export const About = () => {
             <div className='flex flex-col justify-center items-start px-4 pt-20 w-full xl:w-[50%]'>
                 <span className='text-yellow-100 text-md xl:text-xl font-semibold border-[1px] border-yellow-100 p-3 rounded-xl mb-3 titleTime'>Über uns</span>
                 <h1 className='text-yellow-300 text-5xl md:text-7xl font-bold titleTime'>Verändere dein leben!</h1>
-                <p className='text-white text-md xl:text-xl mt-4  w-full textTime'>Unser Ziel ist es, ein qualitativ hochwertiges Boxtraining anzubieten, das unseren Schülern hilft, sich zu verbessern, ihre Ziele zu erreichen und selbstbewusster und gesünder zu leben. Wir trainieren <span className="text-yellow-300">dienstags, donnerstags um 18:30 Uhr </span>  und <span className="text-yellow-300">Sonntags um 12:00 Uhr </span> in Altstetten.
+                <p ref={textRef} className='text-white text-md xl:text-xl mt-4  w-full textTime'>Unser Ziel ist es, ein qualitativ hochwertiges Boxtraining anzubieten, das unseren Schülern hilft, sich zu verbessern, ihre Ziele zu erreichen und selbstbewusster und gesünder zu leben. Wir trainieren <span className="text-yellow-300">dienstags, donnerstags um 18:30 Uhr </span>  und <span className="text-yellow-300">Sonntags um 12:00 Uhr </span> in Altstetten.
                     Bei uns geht&apos;s nicht nur um Technik, sondern auch um Fitness, Spaß und eine coole Gemeinschaft. Jeder kann mitmachen, ganz egal, welches Level du hast.</p>
                 <div className='mt-2 w-full flex justify-center items-center cardTime'>
                     <Card />
--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -1,17 +1,18 @@
 'use client'
 import Image from 'next/image'
 import promotionImg from '../assets/promotion.jpeg'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import '../styles/animations.css'
 
 export const Promotion = () => {
-
+    const titleRef = useRef<HTMLParagraphElement>(null);
+    const textRef = useRef<HTMLParagraphElement>(null);
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
 
-        gsap.fromTo('.boxer',{opacity: 0}, {
+        gsap.fromTo('.boxer', { opacity: 0 }, {
             opacity: 1,
             x: 0,
             scrollTrigger: {
@@ -22,29 +23,51 @@ export const Promotion = () => {
             }
         })
 
-        gsap.fromTo('.textPromotion',{opacity:0}, {
-            opacity: 1,
-            x: 0,
-            duration: 4,
-            scrollTrigger: {
-                trigger: '.promotion',
-                start: 'top 80%',
-                end: 'bottom 80%',
-                scrub: true,
-            }
-        })
+        // Animação do texto
+        const splitText = (element: HTMLParagraphElement) => {
+            const text = element.innerText.split('').map((char, index) => (
+                `<span class="char" key=${index}>${char}</span>`
+            )).join('');
+            element.innerHTML = text;
+        };
 
-        gsap.fromTo('.titlePromotion',{opacity: 0}, {
-            opacity: 1,
-            x: 0,
-            duration: 2,
-            scrollTrigger: {
-                trigger: '.promotion',
-                start: 'top 80%',
-                end: 'bottom 80%',
-                scrub: true,
-            }
-        })
+        if (textRef.current) {
+            splitText(textRef.current);
+            gsap.fromTo(
+                textRef.current.children,
+                { opacity: 0, y: 50 },
+                {
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.01,
+                    duration: 1,
+                    scrollTrigger: {
+                        trigger: '.promotion',
+                        start: 'top 80%',
+                        toggleActions: "play none none reverse",
+                    },
+                }
+            );
+        }
+        if (titleRef.current) {
+            splitText(titleRef.current);
+            gsap.fromTo(
+                titleRef.current.children,
+                { opacity: 0, y: -100 },
+                {
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.01,
+                    duration: 1,
+                    scrollTrigger: {
+                        trigger: '.promotion',
+                        start: 'top 80%',
+                        toggleActions: "play none none reverse",
+                    },
+                }
+            );
+        }
+
 
         return () => {
             gsap.killTweensOf('title')
@@ -57,8 +80,8 @@ export const Promotion = () => {
         <div className='flex flex-col xl:flex-row w-full md:pt-10 promotion'>
             <div className='flex flex-col justify-center items-start px-4 w-full xl:w-[50%] '>
                 <span className='text-yellow-100 text-md xl:text-xl font-semibold border-[1px] border-yellow-100 p-3 rounded-xl mb-3 tra titlePromotion'>Nur für kurze Zeit</span>
-                <h1 className='text-yellow-300 text-5xl md:text-7xl font-bold titlePromotion'>Probetraining <br /> gefällig?</h1>
-                <p className='text-white text-md xl:text-xl mt-4 textPromotion'>
+                <h1 ref={titleRef} className='text-yellow-300 text-5xl md:text-7xl font-bold titlePromotion'>Probetraining <br /> gefällig?</h1>
+                <p ref={textRef} className='text-white text-md xl:text-xl mt-4 textPromotion'>
                     Melde dich einfach per DM oder Nachricht bei uns an und komm zu einem Probetraining vorbei. Wir freuen uns immer über neue Gesichter!
                     Komm vorbei und erlebe, wie viel Spaß Boxen machen kann!
                 </p>

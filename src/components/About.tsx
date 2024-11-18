f92066b37273a21db817a19b0007729234c9275f
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-11-18 23:54:05 +0000

    Removed useRef and associated code for text animation in About and Promotion components.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useRef } from "react";
+import React, { useEffect} from "react";
 import woman from '../assets/woman.jpeg'
 import Image from 'next/image'
 import Card from './ui/Card'
@@ -10,7 +10,7 @@ import '../styles/animations.css'
 
 
 export const About = () => {
-    const textRef = useRef<HTMLParagraphElement>(null);
+
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
@@ -46,33 +46,6 @@ export const About = () => {
             }
         })
 
-        // Animação do texto
-        const splitText = (element: HTMLParagraphElement) => {
-            const text = element.innerText.split('').map((char, index) => (
-                `<span class="char" key=${index}>${char}</span>`
-            )).join('');
-            element.innerHTML = text;
-        };
-
-        if (textRef.current) {
-            splitText(textRef.current);
-            gsap.fromTo(
-                textRef.current.children,
-                { opacity: 0, y: 50 },
-                {
-                    opacity: 1,
-                    y: 0,
-                    stagger: 0.01,
-                    duration: 0.8,
-                    scrollTrigger: {
-                        trigger: '.titleTime',
-                        start: 'top 80%',
-                        toggleActions: "play none none reverse",
-                    },
-                }
-            );
-        }
-
     }, [])
 
     return (
@@ -80,7 +53,7 @@ export const About = () => {
             <div className='flex flex-col justify-center items-start px-4 pt-20 w-full xl:w-[50%]'>
                 <span className='text-yellow-100 text-md xl:text-xl font-semibold border-[1px] border-yellow-100 p-3 rounded-xl mb-3 titleTime'>Über uns</span>
                 <h1 className='text-yellow-300 text-5xl md:text-7xl font-bold titleTime'>Verändere dein leben!</h1>
-                <p ref={textRef} className='text-white text-md xl:text-xl mt-4  w-full textTime'>Unser Ziel ist es, ein qualitativ hochwertiges Boxtraining anzubieten, das unseren Schülern hilft, sich zu verbessern, ihre Ziele zu erreichen und selbstbewusster und gesünder zu leben. Wir trainieren <span className="text-yellow-300">dienstags, donnerstags um 18:30 Uhr </span>  und <span className="text-yellow-300">Sonntags um 12:00 Uhr </span> in Altstetten.
+                <p className='text-white text-md xl:text-xl mt-4  w-full textTime'>Unser Ziel ist es, ein qualitativ hochwertiges Boxtraining anzubieten, das unseren Schülern hilft, sich zu verbessern, ihre Ziele zu erreichen und selbstbewusster und gesünder zu leben. Wir trainieren <span className="text-yellow-300">dienstags, donnerstags um 18:30 Uhr </span>  und <span className="text-yellow-300">Sonntags um 12:00 Uhr </span> in Altstetten.
                     Bei uns geht&apos;s nicht nur um Technik, sondern auch um Fitness, Spaß und eine coole Gemeinschaft. Jeder kann mitmachen, ganz egal, welches Level du hast.</p>
                 <div className='mt-2 w-full flex justify-center items-center cardTime'>
                     <Card />

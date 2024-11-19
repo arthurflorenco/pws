088eb66c15da1c524d1085d266440e11670bbbf1
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-11-19 22:49:42 +0000

    "Updated next.config.ts, added export script to package.json, and removed SVG files from public directory"

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Necessário para imagens em exportações estáticas
+  },
   /* config options here */
 };
 
